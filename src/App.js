b83a5e7a01fb7eb62c338ff655a28ce45f073c98
poramet260241 <oblivion2408@gmail.com>
2019-09-26 00:10:40 +0700

    add button design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from "./WordCard";
 
-const word = ["Dog", "Apple", "Computer", "Power"];
+const word = ["Dog", "Apple", "Computer", "Power", ];
 class App extends Component {
 
   reload = () => {
@@ -12,17 +12,18 @@ class App extends Component {
   render() {
 
     return (
-    <div className = 'App'>
-        <p className = 'Description'>CREATE WORDS FROM THE FOLLOWING CHARACTERS</p>
-        <WordCard value={word[Math.floor((Math.random() * 4))]}/>
-        <p className = 'Status' id = 'roundBar'>ROUND 1</p>
-        <p className = 'Status' id = 'statusBar'></p>
-        <button id = 'newgameBtn' onClick = {this.reload}>New Game</button>
-        <br></br>
-        <br></br>
-        <p className = 'Status' id = 'answerBar'></p>
 
-    </div>
+        <div className = 'App'>
+            <p className = 'Description'>CREATE WORDS FROM THE FOLLOWING CHARACTERS</p>
+            <WordCard value={word[Math.floor((Math.random() * 4))]}/>
+            <p className = 'Status' id = 'roundBar'>ROUND 1</p>
+            <p className = 'Status' id = 'statusBar'></p>
+            <button className = 'button' id = 'newgameBtn' onClick = {this.reload}>New Game</button>
+            <br></br>
+            <br></br>
+            <p className = 'Status' id = 'answerBar'></p>
+
+        </div>
     );
    }
 }
