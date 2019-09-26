fe337c5ad59c8fb38e033471ff9b476c54bbc9b5
poramet260241 <oblivion2408@gmail.com>
2019-09-26 21:43:12 +0700

    change style of card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from "./WordCard";
 
+
 const word = ["Dog","Cat","Bird","Table", "Car", "Game", "Start", "Eyes", "Apple", "Computer", "Power", ];
 class App extends Component {
 
@@ -12,12 +13,11 @@ class App extends Component {
   render() {
 
     return (
-
         <div className = 'App'>
             <p className = 'Description'>CREATE WORDS FROM THE FOLLOWING CHARACTERS</p>
             <WordCard value={word[Math.floor((Math.random() * 4))]}/>
             <p className = 'Status' id = 'roundBar'>ROUND 1</p>
-            <p className = 'Status' id = 'statusBar'></p>
+            <p className = 'Status' id = 'statusBar'> </p>
             <button className = 'button' id = 'newgameBtn' onClick = {this.reload}>New Game</button>
             <p className = 'Status' id = 'answerBar'></p>
 
